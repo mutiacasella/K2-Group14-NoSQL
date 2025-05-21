fea68792b122603d977f6d60b50b1b98583b8d92
sitiamalianurfaidah <150977479+sitiamalianurfaidah@users.noreply.github.com>
2025-05-21 13:37:15 +0700

    Hapus Mongo Test

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,12 +1,12 @@
+// config/db.js
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
         });
-
         console.log("✅ Connected to MongoDB");
     } catch (err) {
         console.error("❌ MongoDB connection error:", err);
